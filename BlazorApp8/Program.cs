91256a4604bc97f3a5c09346f58f3996a494f7d1
Martin Kearn <martinkearn@live.co.uk>
2024-01-28 08:36:03 +0000

    All working stripped back to template

--- BlazorApp8/Program.cs
+++ BlazorApp8/Program.cs
@@ -3,25 +3,34 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 
+//Provided by template
 var builder = WebApplication.CreateBuilder(args);
 
-//Auth services
+//Reads the app settings for AzureAdB2C and registers an authentication service
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
+
+//AddMicrosoftIdentityUI adds the razor pages used for account management, login etc
 builder.Services.AddControllersWithViews()
     .AddMicrosoftIdentityUI();
+
+//Adds CascadingAuthenticationState to service collection to avoid having it in razor.
+//CascadingAuthenticationState exposes authentication data to razor pages and enables things like AuthorizeView
 builder.Services.AddCascadingAuthenticationState();
-builder.Services.AddMicrosoftIdentityConsentHandler();
+
+//Required for Microsoft.Identity.Web.UI
+//Based on https://github.com/dotnet/aspnetcore/issues/52245
 builder.Services.AddRazorPages();
 
+//Provided by template
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddMicrosoftIdentityConsentHandler();
+    .AddInteractiveServerComponents();
 
-
+//Provided by template
 var app = builder.Build();
 
+//Provided by template
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -30,15 +39,14 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+//Provided by template
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthorization();
 app.UseAntiforgery();
-app.MapControllers();
-app.UseAntiforgery();
+
+//Provided by template
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
+//Provided by template
 app.Run();
